4e2c535f53957597110dd13d70c98803589ee938
miunsag <108091043+miunsag@users.noreply.github.com>
2023-12-28 11:06:41 +0100

    Trial 1 - 2023-12-28

--- code/aws-tf-01/main.tf
+++ code/aws-tf-01/main.tf
@@ -23,13 +23,13 @@ resource "aws_subnet" "service-vpc-01-subnet-01" {
   cidr_block = "10.0.1.0/24"
 }
 
-############### CloudWatch logging
-# We add CloudWatch logging at checkov suggestion
-resource "aws_kms_key" "our_kms_key" {
-  # checkov:skip=CKV_AWS_7: ADD REASON
-  description             = "Our KMS key"
-  deletion_window_in_days = 8
-}
+# ############### CloudWatch logging
+# # We add CloudWatch logging at checkov suggestion
+# resource "aws_kms_key" "our_kms_key" {
+#   # checkov:skip=CKV_AWS_7: ADD REASON
+#   description             = "Our KMS key"
+#   deletion_window_in_days = 8
+# }
 
 resource "aws_flow_log" "vpc-01-flow-log" {
   iam_role_arn    = aws_iam_role.our_log_iam_role.arn
@@ -39,32 +39,50 @@ resource "aws_flow_log" "vpc-01-flow-log" {
 }
 
 resource "aws_cloudwatch_log_group" "our_cloudwatch_log_group" {
-  name              = "our_cloudwatch_log_group"
-  kms_key_id        = aws_kms_key.our_kms_key.id
+  # checkov:skip=CKV_AWS_158: ADD REASON
+  name = "our_cloudwatch_log_group"
+  #kms_key_id        = aws_kms_key.our_kms_key.id
   retention_in_days = 5
 }
 
 resource "aws_iam_role" "our_log_iam_role" {
   name               = "our_log_iam_role"
-  assume_role_policy = data.aws_iam_policy_document.log_policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_logger_role.json
 }
 
-data "aws_iam_policy_document" "log_policy" {
-  # checkov:skip=CKV_AWS_111: Do not have sensibility about why this is wrong ATM
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:DescribeLogGroups",
-      "logs:DescribeLogStreams",
-    ]
-
-    resources = ["*"]
-  }
+data "aws_iam_policy" "log_policy" {
+  provider = aws.destination
+  name     = "CloudWatchLogsFullAccess"
 }
+
+resource "aws_iam_role" "assume_logger_role" {
+  provider            = aws.destination
+  name                = "assume_logger_role"
+  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = [data.aws_iam_policy.log_policy.arn]
+}
+
+
+# data "aws_caller_identity" "current" {}
+
+# data "aws_iam_policy_document" "log_policy" {
+#   checkov:skip=CKV_AWS_111: Do not have sensibility about why this is wrong ATM
+#   statement {
+#     effect = "Allow"
+
+#     actions = [
+#       "logs:*",
+#       "cloudwatch:GenerateQuery"
+#     ]
+
+#     principals {
+#       type        = "AWS"
+#       identifiers = [data.aws_caller_identity.current.id]
+#     }
+
+#     resources = ["*"]
+#   }
+# }
 
 ############### Network Security Group (can we reuse for more VPCs?)
 resource "aws_default_security_group" "default" {
